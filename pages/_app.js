303d9d2b7ebc06d1488e55f4f4db93a0f7a1b81e
ha-nagisa <nagisa18222@icloud.com>
2021-06-12 11:10:07 +0900

    レイアウト調整の続き

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import "../styles/globals.css";
 import { library, config } from "@fortawesome/fontawesome-svg-core";
-import { faBorderAll, faList, faSortNumericDown, faSortNumericUp } from "@fortawesome/free-solid-svg-icons";
+import { faBorderAll, faList, faSortNumericDown, faSortNumericUp, faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 config.autoAddCss = false;
-library.add(faList, faBorderAll, faSortNumericDown, faSortNumericUp);
+library.add(faList, faBorderAll, faSortNumericDown, faSortNumericUp, faArrowUp);
 
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
