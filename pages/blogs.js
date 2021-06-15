09efaaf51b901419ea81538c5b2c33dc66a0bd2e
ha-nagisa <nagisa18222@icloud.com>
2021-06-15 16:50:42 +0900

    ページネーションの実装

--- pages/blogs.js
+++ pages/blogs.js
@@ -1,14 +1,52 @@
+import { useGetBlogsPages } from "actions/pagination";
 import CardItem from "components/cardItem";
 import CardListItem from "components/cardListItem";
 import LowerPageLayout from "layout/lowerPageLayout";
-import { getAllBlogs } from "lib/api";
+import { getAllBlogs, getPaginatedBlogs } from "lib/api";
 import { useState } from "react";
+
+export const BlogList = ({ data = [], filter }) => {
+  return data.map((page) =>
+    page.map((blog) =>
+      filter.view.list ? (
+        <CardListItem
+          key={blog.slug}
+          title={blog.title}
+          summary={blog.summary}
+          date={blog.date}
+          image={blog.coverImage}
+          slug={blog.slug}
+        />
+      ) : (
+        <CardItem
+          key={blog.slug}
+          title={blog.title}
+          summary={blog.summary}
+          date={blog.date}
+          image={blog.coverImage}
+          slug={blog.slug}
+        />
+      )
+    )
+  );
+};
 
 const Blogs = ({ blogs }) => {
   const [filter, setFilter] = useState({
     view: { list: 0 },
     date: { asc: 0 },
   });
+
+  const {
+    data,
+    size,
+    setSize,
+    isLoadingInitialData,
+    isLoadingMore,
+    isEmpty,
+    isReachingEnd,
+    isRefreshing,
+  } = useGetBlogsPages({ filter });
 
   return (
     <LowerPageLayout
@@ -21,27 +59,22 @@ const Blogs = ({ blogs }) => {
       <section className="text-gray-600 body-font">
         <div className="container px-5 pb-24 mx-auto">
           <div className="flex flex-wrap -m-4">
-            {blogs.map((blog) =>
-              filter.view.list ? (
-                <CardListItem
-                  key={blog.slug}
-                  title={blog.title}
-                  summary={blog.summary}
-                  date={blog.date}
-                  image={blog.coverImage}
-                  slug={blog.slug}
-                />
-              ) : (
-                <CardItem
-                  key={blog.slug}
-                  title={blog.title}
-                  summary={blog.summary}
-                  date={blog.date}
-                  image={blog.coverImage}
-                  slug={blog.slug}
-                />
-              )
-            )}
+            <BlogList data={data || [blogs]} filter={filter} />
+          </div>
+          <div className="block text-center mt-12">
+            <button
+              className={`inline-block border-2 text-white px-4 py-3 rounded-lg  focus:outline-none ${
+                isLoadingMore || isReachingEnd
+                  ? "cursor-default bg-gray-400 border-gray-400"
+                  : "bg-gray-800 hover:bg-white hover:text-gray-800 hover:border-gray-800"
+              }`}
+              onClick={() => setSize(size + 1)}
+              disabled={isLoadingMore || isReachingEnd}
+              size="lg"
+              variant="outline-secondary"
+            >
+              {isLoadingMore ? "読み込み中....." : isReachingEnd ? "すべて表示済み" : "さらに表示"}
+            </button>
           </div>
         </div>
       </section>
@@ -52,10 +85,11 @@ const Blogs = ({ blogs }) => {
 export default Blogs;
 
 export async function getStaticProps() {
-  const blogs = await getAllBlogs();
+  const blogs = await getPaginatedBlogs({ offset: 0, date: "desc" });
   return {
     props: {
       blogs,
     },
+    revalidate: 1,
   };
 }
--- pages/works.js
+++ pages/works.js
@@ -1,14 +1,53 @@
+import { useGetWorksPages } from "actions/pagination";
 import CardItem from "components/cardItem";
 import CardListItem from "components/cardListItem";
 import LowerPageLayout from "layout/lowerPageLayout";
-import { getAllWorks } from "lib/api";
+import { getPaginatedWorks } from "lib/api";
 import { useState } from "react";
+
+export const WorkList = ({ data = [], filter }) => {
+  return data.map((page) =>
+    page.map((work) =>
+      filter.view.list ? (
+        <CardListItem
+          key={work.slug}
+          title={work.title}
+          summary={work.summary}
+          date={work.date}
+          image={work.coverImage}
+          slug={work.slug}
+        />
+      ) : (
+        <CardItem
+          key={work.slug}
+          title={work.title}
+          summary={work.summary}
+          date={work.date}
+          image={work.coverImage}
+          slug={work.slug}
+        />
+      )
+    )
+  );
+};
 
 const Works = ({ works }) => {
   const [filter, setFilter] = useState({
     view: { list: 0 },
     date: { asc: 0 },
   });
+
+  const {
+    data,
+    size,
+    setSize,
+    isLoadingInitialData,
+    isLoadingMore,
+    isEmpty,
+    isReachingEnd,
+    isRefreshing,
+  } = useGetWorksPages({ filter });
+
   return (
     <LowerPageLayout
       wrapperClass="-mt-64"
@@ -20,27 +59,22 @@ const Works = ({ works }) => {
       <section className="text-gray-600 body-font">
         <div className="container px-5 pb-24 mx-auto">
           <div className="flex flex-wrap -m-4">
-            {works.map((work) =>
-              filter.view.list ? (
-                <CardListItem
-                  key={work.slug}
-                  title={work.title}
-                  summary={work.summary}
-                  date={work.date}
-                  image={work.coverImage}
-                  slug={work.slug}
-                />
-              ) : (
-                <CardItem
-                  key={work.slug}
-                  title={work.title}
-                  summary={work.summary}
-                  date={work.date}
-                  image={work.coverImage}
-                  slug={work.slug}
-                />
-              )
-            )}
+            <WorkList data={data || [works]} filter={filter} />
+          </div>
+          <div className="block text-center mt-12">
+            <button
+              className={`inline-block border-2 text-white px-4 py-3 rounded-lg  focus:outline-none ${
+                isLoadingMore || isReachingEnd
+                  ? "cursor-default bg-gray-400 border-gray-400"
+                  : "bg-gray-800 hover:bg-white hover:text-gray-800 hover:border-gray-800"
+              }`}
+              onClick={() => setSize(size + 1)}
+              disabled={isLoadingMore || isReachingEnd}
+              size="lg"
+              variant="outline-secondary"
+            >
+              {isLoadingMore ? "読み込み中....." : isReachingEnd ? "すべて表示済み" : "さらに表示"}
+            </button>
           </div>
         </div>
       </section>
@@ -51,10 +85,11 @@ const Works = ({ works }) => {
 export default Works;
 
 export async function getStaticProps() {
-  const works = await getAllWorks();
+  const works = await getPaginatedWorks({ offset: 0, date: "desc" });
   return {
     props: {
       works,
     },
+    revalidate: 1,
   };
 }
