4e739940ca3e4d5eab92caafcf1cfa9817ac5fb4
ha-nagisa <nagisa18222@icloud.com>
2021-06-18 19:24:38 +0900

    classをclassNameに変更

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -13,10 +13,10 @@ const BlogDetail = ({ blog, preview }) => {
 
   if (router.isFallback) {
     return (
-      <div class="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-green-800 opacity-75 flex flex-col items-center justify-center">
-        <div class="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
-        <h2 class="text-center text-white text-xl font-semibold">読み込んでいます。</h2>
-        <p class="w-1/3 text-center text-white">少々お待ちください！</p>
+      <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-green-800 opacity-75 flex flex-col items-center justify-center">
+        <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
+        <h2 className="text-center text-white text-xl font-semibold">読み込んでいます。</h2>
+        <p className="w-1/3 text-center text-white">少々お待ちください！</p>
       </div>
     );
   }
