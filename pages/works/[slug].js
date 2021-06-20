a38cd393df6ef935f8a1ac563944579be919e16f
ha-nagisa <nagisa18222@icloud.com>
2021-06-20 18:04:09 +0900

    SEOの設定

--- pages/works/[slug].js
+++ pages/works/[slug].js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import dayjs from "dayjs";
 import { useRouter } from "next/router";
+import { NextSeo } from "next-seo";
+import Link from "next/link";
 
 import ArticleContent from "components/ArticleContent";
 import ArticleDetailLayout from "layout/articleDetailLayout";
 import { getAllWorks, getWorkBySlug, urlFor } from "lib/api";
 import PreviewAlert from "components/PreviewAlert";
-import Link from "next/link";
 
 const BlogDetail = ({ work, preview }) => {
   const router = useRouter();
@@ -22,58 +23,82 @@ const BlogDetail = ({ work, preview }) => {
   }
 
   return (
-    <ArticleDetailLayout>
-      <div className="-mt-40 sm:-mt-72">
-        <div className="container mx-auto w-11/12 text-center md:p-5 xl:max-w-5xl mt-8 sm:-mt-8">
-          <div className="-mr-2 text-center sm:mr-0 md:p-10">
-            <Image
-              width={820}
-              height={400}
-              className="inline-block w-full max-w-5xl max-h-96 rounded-md object-cover object-center"
-              src={urlFor(work.coverImage).height(600).url()}
-              alt="blog"
-            />
-          </div>
-          <div className="-ml-2 -mt-12 text-left sm:-mt-28 sm:mr-0 relative">
-            <div className="block">
-              <p className="inline-block px-4 py-3 max-w-md text-gray-800 text-xl font-bold bg-white border-2 border-gray-800 rounded-md sm:px-6 sm:py-5 sm:text-2xl md:text-3xl lg:max-w-3xl lg:text-5xl">
-                {work.title}
-              </p>
+    <>
+      <NextSeo
+        title={`${work.title} | nagisa-profile`}
+        description={`${work.summary}`}
+        canonical={`https://nagisa-profile.com/blogs/${work.slug}`}
+        openGraph={{
+          type: "article",
+          url: `https://nagisa-profile.com/blogs/${work.slug}`,
+          title: `${work.title} | nagisa-profile`,
+          description: `${work.summary}`,
+          images: [
+            {
+              url: urlFor(work.coverImage).height(600).url(),
+              alt: `${work.title}`,
+            },
+          ],
+        }}
+        twitter={{
+          handle: "@sora_pi_2",
+          site: "@sora_pi_2",
+          cardType: "summary_large_image",
+        }}
+      />
+      <ArticleDetailLayout>
+        <div className="-mt-40 sm:-mt-72">
+          <div className="container mx-auto w-11/12 text-center md:p-5 xl:max-w-5xl mt-8 sm:-mt-8">
+            <div className="-mr-2 text-center sm:mr-0 md:p-10">
+              <Image
+                width={820}
+                height={400}
+                className="inline-block w-full max-w-5xl max-h-96 rounded-md object-cover object-center"
+                src={urlFor(work.coverImage).height(600).url()}
+                alt="blog"
+              />
             </div>
-            <div className="block">
-              <p className="text-md inline-block px-3 py-2 text-gray-800 font-medium bg-white border-2 border-t-0 border-gray-800 rounded-b-md sm:text-xl md:text-3xl">
-                {dayjs(work.date).format("YYYY/MM/DD")}
-              </p>
+            <div className="-ml-2 -mt-12 text-left sm:-mt-28 sm:mr-0 relative">
+              <div className="block">
+                <p className="inline-block px-4 py-3 max-w-md text-gray-800 text-xl font-bold bg-white border-2 border-gray-800 rounded-md sm:px-6 sm:py-5 sm:text-2xl md:text-3xl lg:max-w-3xl lg:text-5xl">
+                  {work.title}
+                </p>
+              </div>
+              <div className="block">
+                <p className="text-md inline-block px-3 py-2 text-gray-800 font-medium bg-white border-2 border-t-0 border-gray-800 rounded-b-md sm:text-xl md:text-3xl">
+                  {dayjs(work.date).format("YYYY/MM/DD")}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
 
-        <section className="body-font p-5 text-gray-600">
-          <div className="container mx-auto md:px-20 xl:max-w-5xl">
-            <div className="text-right border-b-2 border-gray-800 pb-2 mb-5 ">
-              <Link href="/about">
-                <a className="inline-block">
-                  <div className="mr-2 inline-block align-middle">{work.author.name}</div>
-                  <div className="inline-block align-middle">
-                    <Image
-                      width={40}
-                      height={40}
-                      className="rounded-full object-cover object-center"
-                      src={work.author.avatar}
-                      alt="blog"
-                    />
-                  </div>
-                </a>
-              </Link>
+          <section className="body-font p-5 text-gray-600">
+            <div className="container mx-auto md:px-20 xl:max-w-5xl">
+              <div className="text-right border-b-2 border-gray-800 pb-2 mb-5 ">
+                <Link href="/about">
+                  <a className="inline-block">
+                    <div className="mr-2 inline-block align-middle">{work.author.name}</div>
+                    <div className="inline-block align-middle">
+                      <Image
+                        width={40}
+                        height={40}
+                        className="rounded-full object-cover object-center"
+                        src={work.author.avatar}
+                        alt="blog"
+                      />
+                    </div>
+                  </a>
+                </Link>
+              </div>
             </div>
-          </div>
-          <div className="container mx-auto pb-24 md:px-20 xl:max-w-5xl">
-            {preview ? <PreviewAlert /> : null}
-            {work.content && <ArticleContent content={work.content} />}
-          </div>
-        </section>
-      </div>
-    </ArticleDetailLayout>
+            <div className="container mx-auto pb-24 md:px-20 xl:max-w-5xl">
+              {preview ? <PreviewAlert /> : null}
+              {work.content && <ArticleContent content={work.content} />}
+            </div>
+          </section>
+        </div>
+      </ArticleDetailLayout>
+    </>
   );
 };
 
